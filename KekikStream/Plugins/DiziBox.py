53df74ece019b5dc4d511abf1b03ccecd00c0385
keyiflerolsun <keyiflerolsun@users.noreply.github.com>
2025-07-12 18:40:43 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- KekikStream/Plugins/DiziBox.py
+++ KekikStream/Plugins/DiziBox.py
@@ -8,7 +8,7 @@ import re, urllib.parse, base64, contextlib, asyncio
 class DiziBox(PluginBase):
     name        = "DiziBox"
     language    = "tr"
-    main_url    = "https://www.dizibox.live"
+    main_url    = "https://www.dizibox.so"
     favicon     = f"https://www.google.com/s2/favicons?domain={main_url}&sz=64"
     description = "Yabancı Dizi izle, Tüm yabancı dizilerin yeni ve eski sezonlarını full hd izleyebileceğiniz elit site."
 
