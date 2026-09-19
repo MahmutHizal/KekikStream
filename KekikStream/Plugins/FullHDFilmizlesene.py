37d5c020431fe2a28cd6d46e3e604a8e31767991
keyiflerolsun <keyiflerolsun@users.noreply.github.com>
2026-09-19 13:08:42 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- KekikStream/Plugins/FullHDFilmizlesene.py
+++ KekikStream/Plugins/FullHDFilmizlesene.py
@@ -8,7 +8,7 @@ import json, re
 class FullHDFilmizlesene(PluginBase):
     name        = "FullHDFilmizlesene"
     language    = "tr"
-    main_url    = "https://www.fullhdfilmizlesene.de"
+    main_url    = "https://www.fullhdfilmizlesene.now"
     favicon     = f"https://www.google.com/s2/favicons?domain={main_url}&sz=64"
     description = "Sinema zevkini evinize kadar getirdik. Türkiye'nin lider Film sitesinde, en yeni filmleri Full HD izleyin."
 
